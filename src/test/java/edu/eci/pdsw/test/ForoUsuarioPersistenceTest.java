ca44292db56c0d500b6a16529c8849917ed762c3
Hernan Felipe Losada <hernan.losada96@gmail.com>
2016-10-05 16:54:03 -0500

    Primera Version pruebas

---
+++ src/test/java/edu/eci/pdsw/test/ForoUsuarioPersistenceTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author 2105409
+ */
+public class ForoUsuarioPersistenceTest {
+    
+}
